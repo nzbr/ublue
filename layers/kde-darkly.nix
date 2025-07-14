06ea396fe9d3a89da1de21cdcff1895364da16c3
Wire Jansen <mail@nzbr.de>
2025-07-14 14:29:40 +0200

    kde-darkly: enable copr through dnf

--- layers/kde-darkly.nix
+++ layers/kde-darkly.nix
@@ -1,21 +1,9 @@
-{ lib, ... }:
-let
-  fedora-version = "42";
-  repo = builtins.fetchurl {
-    url = "https://copr.fedorainfracloud.org/coprs/deltacopy/darkly/repo/fedora-41/deltacopy-darkly-fedora-${fedora-version}.repo";
-    sha256 = "sha256:1qc64fzbmdamq5vjr5q1r24qc7csvdpldrxrb0dawiix78nafwjd";
-  };
-in
+{ ... }:
 {
   name = "kde-darkly";
 
-  build = ''
-    cp ${repo} ./deltacopy-darkly-fedora-${fedora-version}.repo
-  '';
-
   install = ''
-    install -Dm644 ./deltacopy-darkly-fedora-${fedora-version}.repo /etc/yum.repos.d/deltacopy-darkly-fedora-${fedora-version}.repo
-
+    dnf copr enable -y deltacopy/darkly
     rpm-ostree install darkly
   '';
 }
