7752d84f42e1107549155cf18d89173ef7204dc0
Wire Jansen <mail@nzbr.de>
2025-09-30 17:54:54 +0200

    ollama: use regular rpmbuild

--- layers/ollama.nix
+++ layers/ollama.nix
@@ -1,46 +1,71 @@
 { lib
-, rpmpack
 , hostPlatform
+, writeText
 , withRocm ? false
 , ... 
 }:
 let
   inherit (lib)
+    concatStringsSep
     last
+    length
     optionalString
+    removePrefix
     replaceString
     splitString
-    removePrefix
     ;
 
   baseUrl = "https://github.com/ollama/ollama/releases/download/v0.11.10";
 
-  mkRPM = file: extraOpts:
+  mkRPM = file: deps:
+  let
+    name = replaceString "-linux-amd64" "" file;
+    version = removePrefix "v" (last (splitString "/" baseUrl));
+    specfile = writeText "${name}.spec" ''
+      AutoReqProv: no
+
+      Name: ${name}
+      Version: ${version}
+      Release: 1%{?dist}
+      BuildArch: x86_64
+      Summary: ${name}
+      License: MIT
+      ${optionalString (length deps > 0) "Requires: ${concatStringsSep ", " deps}"}
+
+      %description
+      ${name}
+  
+      %install
+      export QA_RPATHS=0x0030
+      rm -rf $RPM_BUILD_ROOT
+      mkdir -p $RPM_BUILD_ROOT/usr
+      tar -I pigz -C $RPM_BUILD_ROOT/usr -xvf %{_sourcedir}/${file}.tgz
+
+      %clean
+      rm -rf $RPM_BUILD_ROOT
+
+      %files
+    '';
+  in
   ''
-    wget ${baseUrl}/${file}.tgz -O ${file}.tgz
-    mkdir -p unpack/usr
-    tar -I pigz -C unpack/usr -xvf ${file}.tgz
-    rm ${file}.tgz
-    tar -cf ${file}.tar -C unpack .
-    rm -rf unpack
-    ${rpmpack}/bin/tar2rpm \
-      -use_dir_allowlist \
-      -name ${replaceString "-linux-amd64" "" file} \
-      -version ${removePrefix "v" (last (splitString "/" baseUrl))} \
-      -release tar2rpm \
-      -arch x86_64 \
-      -file ${file}.rpm \
-      ${extraOpts} \
-      ${file}.tar
-    rm ${file}.tar
+    mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+    wget ${baseUrl}/${file}.tgz -O rpmbuild/SOURCES/${file}.tgz
+    cp ${specfile} rpmbuild/SPECS/${name}.spec
+    tar -I pigz -tf rpmbuild/SOURCES/${file}.tgz | sed -E 's|^\.?/?|/usr/|;/\/$/d' >> rpmbuild/SPECS/${name}.spec
+    cat rpmbuild/SPECS/${name}.spec
+    rpmbuild --define "_topdir $PWD/rpmbuild" -bb rpmbuild/SPECS/${name}.spec
+    mv rpmbuild/RPMS/x86_64/*.rpm $PWD/${file}.rpm
+    rm -rf rpmbuild
   '';
 in
 {
   name = "Ollama";
 
   build = ''
-    ${mkRPM "ollama-linux-amd64" ""}
-    ${optionalString withRocm (mkRPM "ollama-linux-amd64-rocm" "-requires rocm -requires ollama")}
+    rpm-ostree install rpmdevtools
+
+    ${mkRPM "ollama-linux-amd64" []}
+    ${optionalString withRocm (mkRPM "ollama-linux-amd64-rocm" ["rocm" "ollama"])}
   '';
 
   install = ''
