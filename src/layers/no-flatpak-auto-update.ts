4f0bb8dcbc38209c78f42bcd3ec81f5d803979d5
Wire Jansen <mail@nzbr.de>
2026-08-24 11:19:03 +0200

    no-flatpak-auto-update: also prevent updates through UUPD

--- src/layers/no-flatpak-auto-update.ts
+++ src/layers/no-flatpak-auto-update.ts
@@ -1,9 +1,23 @@
-import { GenericLayer } from "../lib";
+import { Container } from "@dagger.io/dagger";
+import { Layer } from "../lib";
 
-export class NoFlatpakAutoUpdateLayer extends GenericLayer {
+const uupdConfigPath = "/etc/uupd/config.json";
+
+export class NoFlatpakAutoUpdateLayer implements Layer {
     name = "no-flatpak-auto-update";
 
-    installScript = `
-        rm -f /etc/systemd/system/timers.target.wants/flatpak-system-update.timer
-    `;
+    async install(
+        buildContainer: Container,
+        targetContainer: Container,
+    ): Promise<Container> {
+        const uupdConfig: any = JSON.parse(await targetContainer.file(uupdConfigPath).contents());
+
+        uupdConfig.modules ??= {};
+        uupdConfig.modules.flatpak ??= {};
+        uupdConfig.modules.flatpak.disable = true;
+
+        return targetContainer
+            .withoutFile("/etc/systemd/system/timers.target.wants/flatpak-system-update.timer")
+            .withNewFile(uupdConfigPath, JSON.stringify(uupdConfig, null, 2));
+    }
 }
