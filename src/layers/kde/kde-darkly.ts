e464f5633e0b064ecb636608e3ba6a191f82119c
Wire Jansen <mail@nzbr.de>
2026-09-01 22:50:01 +0200

    kde-darkly: install from terra instead of COPR

--- src/layers/kde/kde-darkly.ts
+++ src/layers/kde/kde-darkly.ts
@@ -4,7 +4,8 @@ export class KdeDarklyLayer extends GenericLayer {
     name = "kde-darkly";
 
     installScript = `
-        dnf copr enable -y deltacopy/darkly
+        # darkly is not in the Fedora repos, only in terra, so that layer has to
+        # be included before this one
         dnf install -y darkly
     `;
 }
